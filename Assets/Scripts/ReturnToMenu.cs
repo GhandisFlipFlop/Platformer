62fca0b6d92bc4cabe1e76cf49f3fb9c0fff4501
GhandisFlipFlop <43068438+GhandisFlipFlop@users.noreply.github.com>
2019-01-22 14:44:19 +0000

    Added sounds to game as well as score requirement for doors

---
+++ Assets/Scripts/ReturnToMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// allows us to use scene management
+using UnityEngine.SceneManagement;
+
+public class ReturnToMenu : MonoBehaviour {
+
+
+    public void StartGame()
+    {
+
+        //Reset Life Counter//
+        PlayerPrefs.DeleteKey("lives");
+
+
+        //Reset the Score Counter//
+        PlayerPrefs.DeleteKey("score");
+
+
+        //Load the first Level//
+        SceneManager.LoadScene("StartScreen");
+
+
+    }
+}
